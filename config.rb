fe0574e2b7ee421a418372934a002b572fdb273c
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-02-04 21:33:14 +0100

    utilisation de sass

---
+++ config.rb
@@ -0,0 +1,11 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+
+output_style = :compact # :expanded or :nested or :compact or :compressed
+
+relative_assets = true
+
+line_comments = false
