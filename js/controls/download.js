ec1534e4ebd89a10ec52219c31bc8d5d5a2b77d0
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-03-20 22:36:01 +0100

    ajout d'un controle "download" pour télécharger en un clic le dessin

---
+++ js/controls/download.js
@@ -0,0 +1,12 @@
+DrawingBoard.Control.Download = function(drawingBoard) {
+	this.board = drawingBoard || null;
+	var that = this;
+	var el = '<div class="drawing-board-control drawing-board-control-download">';
+	el += '<button class="drawing-board-control-download-button" title="Télécharger">⤓</button>';
+	el += '</div>';
+	this.$el = $(el);
+	this.$el.on('click', '.drawing-board-control-download-button', function(e) {
+		that.board.downloadImg();
+		e.preventDefault();
+	});
+};
