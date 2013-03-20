74610524e620fab61b80ce10d28d57b0524bc0a1
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-03-20 22:07:29 +0100

    ajout de méthode reset au controle size

--- js/controls/size.js
+++ js/controls/size.js
@@ -5,21 +5,25 @@ DrawingBoard.Control.Size = function(drawingBoard) {
 		'<input type="range" min="1" max="50" value="10" class="drawing-board-control-size-input">' +
 		'<span class="drawing-board-control-size-label"></span>' +
 		'</div>';
-	
+
 	this.$el = $(tpl);
 	this.$el.on('change', 'input', function(e) {
 		that.updateView($(this).val());
 		e.preventDefault();
 	});
-	this.updateView(this.$el.find('input').val());
 };
 
-DrawingBoard.Control.Size.prototype.updateView = function(val) {
-	this.board.ctx.lineWidth = val;
-	this.$el.find('.drawing-board-control-size-label').css({
-		width: val + 'px',
-		height: val + 'px',
-		borderRadius: val + 'px'
-	});
-	this.$el.attr('title', 'Taille du pinceau : ' + val);
+DrawingBoard.Control.Size.prototype = {
+	reset: function() {
+		this.updateView(this.$el.find('input').val());
+	},
+	updateView: function(val) {
+		this.board.ctx.lineWidth = val;
+		this.$el.find('.drawing-board-control-size-label').css({
+			width: val + 'px',
+			height: val + 'px',
+			borderRadius: val + 'px'
+		});
+		this.$el.attr('title', 'Taille du pinceau : ' + val);
+	}
 };
