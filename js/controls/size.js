61f1acdcee33eb77c6cf1b2ca1e294cbd08bb390
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-03-20 23:43:14 +0100

    on a un div inner pour pouvoir positionner en relative/absolute FF aime pas le display table cell et les positions

--- js/controls/size.js
+++ js/controls/size.js
@@ -1,10 +1,10 @@
 DrawingBoard.Control.Size = function(drawingBoard) {
 	this.board = drawingBoard || null;
 	var that = this;
-	var tpl = '<div class="drawing-board-control drawing-board-control-size">' +
+	var tpl = '<div class="drawing-board-control drawing-board-control-size"><div class="drawing-board-control-inner">' +
 		'<input type="range" min="1" max="50" value="10" class="drawing-board-control-size-input">' +
 		'<span class="drawing-board-control-size-label"></span>' +
-		'</div>';
+		'</div></div>';
 
 	this.$el = $(tpl);
 	this.$el.on('change', 'input', function(e) {
