085ff841e37a7301c5a19f7e4bc7bda5db11142f
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-08-31 23:04:43 +0200

    v0.4: we have drawing modes with pencil/eraser/filler + no img in css

--- README.md
+++ README.md
@@ -2,14 +2,7 @@
 
 This is a canvas based drawing app that you can integrate easily on your website.
 
-drawingboard.js consists of a blank canvas surrounded by a few UI elements that control it:
-
-* a color picker,
-* a background color picker,
-* a pencil size chooser,
-* an eraser,
-* navigation buttons to undo or redo lines,
-* a reset button to put the canvas back to its original blank state
+drawingboard.js consists of a blank canvas surrounded by a few UI elements that control it: a color picker, a pencil, a paint can, an eraser, a pencil size chooser, navigations and reset buttons.
 
 You can draw with mouse or touch on pretty much [every browser that supports `<canvas>`](http://caniuse.com/#feat=canvas). Didn't test that much on IE but hey, WIP.
 
@@ -53,13 +46,13 @@ The drawingboard is tied to an HTML element with an #id. Set the dimensions of t
 When instantiating the drawingboard, you can pass a few options as the 2nd parameter in an object:
 
 * `controls`: an array containing the list of controls automatically loaded with the board. The 'Color', 'DrawingMode', 'Size' and 'Navigation' controls are loaded by default. You can pass an object instead of a string to pass control options (ie `['Color', { Navigation: { reset: false }}]`).
-* `controlsPosition`: define where to put the controls: at the "top" or "bottom" of the canvas, aligned to "left"/"right"/"center". "top left" by default.
-* `color`: the board's pencil color. `#000000` (black) by default.
+* `controlsPosition`: define where to put the controls: at the "top" or "bottom" of the canvas, aligned to "left"/"right"/"center". `"top left"` by default.
+* `color`: the board's pencil color. `"#000000"` (black) by default.
 * `size`: the board's pencil size (integer). `1`px radius by default.
-* `background`: the board's background. Give an hex/rgb/hsl value for a color, `false` to have nothing (transparent board). Anything else will be seen as an image. `#fff` (white) by default.
-* `eraserColor`: color of the eraser tool. Set to `background` so that the eraser takes the background color, `transparent` to make transparent lines or set any other color directly (rgb, hsl, #). `background` by default.
-* `webStorage`: do we enable webStorage support? can be `session`, `local` or false. The drawing is saved in sessionStorage or localStorage and restored when you come back on it. `session` by default.
-* `droppable`: do we allow the user to drop an image on the board to draw on it? false by default.
+* `background`: the board's background. Give an hex/rgb/hsl value for a color, `false` to have nothing (transparent board). Anything else will be seen as an image. `"#fff"` (white) by default.
+* `eraserColor`: color of the eraser tool. Set to `"background"` so that the eraser takes the background color, `"transparent"` to make transparent lines or set any other color directly (rgb, hsl, #). `"background"` by default.
+* `webStorage`: do we enable webStorage support? can be `"session"`, `"local"` or false. The drawing is saved in sessionStorage or localStorage and restored when you come back on it. `"session"` by default.
+* `droppable`: do we allow the user to drop an image on the board to draw on it? `false` by default.
 * `enlargeYourContainer`: how should be sized the drawingboard? When `true`, the CSS width and height will be set on the final board's *canvas*, ie the drawing zone. In the example above, that means the board's container will be taller than 400px because of the controls height. If `false`, the CSS width and height will be set on the board's container. That means the addition of the canvas and the controls will be 400px high. `false` by default.
 * `errorMessage`: html string to put in the board's element on browsers that don't support canvas.
 
@@ -71,9 +64,9 @@ The drawingboard has a few simple controls loaded by default, but you can easily
 
 ### Included controls
 
-* `DrawingBoard.Control.Color`: a color picker. You can choose with the `background` option (false by default) if you set the pencil or the background color.
-* `DrawingBoard.Control.Size`: a pencil size chooser. Choose your `type` in the options: `dropdown` is a simple dropdown menu, whereas `range` uses a range input. Default to `auto`: if the browser supports the range input it will use it, otherwise it will use the dropdown menu. As seen in the example page, you can set the type to `range` and add a [range input polyfill](https://github.com/freqdec/fd-slider) if you want it on [every browser](http://caniuse.com/#feat=input-range).
-* `DrawingBoard.Control.DrawingMode`: show buttons to draw with the `pencil` (normal mode) and an `eraser`. You can choose which buttons to show with the options.
+* `DrawingBoard.Control.Color`: the color picker.
+* `DrawingBoard.Control.Size`: a pencil size chooser. Choose your `type` in the options: `"dropdown"` is a simple dropdown menu, whereas `"range"` uses a range input. Default to `"auto"`: if the browser supports the range input it will use it, otherwise it will use the dropdown menu. As seen in the example page, you can set the type to `"range"` and add a [range input polyfill](https://github.com/freqdec/fd-slider) if you want it on [every browser](http://caniuse.com/#feat=input-range).
+* `DrawingBoard.Control.DrawingMode`: show buttons to draw with the `"pencil"` (normal mode), the `"filler"` (the paint can) and an `"eraser"`. You can choose which buttons to show with the options.
 * `DrawingBoard.Control.Navigation`: undo, redo actions and reset the canvas to blank with 3 buttons. You can choose to show or hide each button individually with options.
 * `DrawingBoard.Control.Download`: show a button to download current drawing *(not loaded by default)*.
 
--- dist/drawingboard.nocontrol.js
+++ dist/drawingboard.nocontrol.js
@@ -1,4 +1,4 @@
-/* drawingboard.js v0.3.0 - https://github.com/Leimi/drawingboard.js
+/* drawingboard.js v0.4.0 - https://github.com/Leimi/drawingboard.js
 * Copyright (c) 2013 Emmanuel Pelletier
 * Licensed MIT */
 window.DrawingBoard = {};
@@ -48,6 +48,7 @@ DrawingBoard.Board = function(id, opts) {
 
 	this.canvas = this.dom.$canvas.get(0);
 	this.ctx = this.canvas && this.canvas.getContext && this.canvas.getContext('2d') ? this.canvas.getContext('2d') : null;
+	this.color = this.opts.color;
 
 	if (!this.ctx) {
 		if (this.opts.errorMessage)
@@ -74,7 +75,7 @@ DrawingBoard.Board = function(id, opts) {
 
 
 DrawingBoard.Board.defaultOpts = {
-	controls: ['Color', { Color: { background: true } }, 'DrawingMode', 'Size', 'Navigation'],
+	controls: ['Color', 'DrawingMode', 'Size', 'Navigation'],
 	controlsPosition: "top left",
 	color: "#000000",
 	size: 1,
@@ -112,7 +113,7 @@ DrawingBoard.Board.prototype = {
 
 		if (opts.background) this.resetBackground(this.opts.background, opts.history);
 
-		if (opts.color) this.ctx.strokeStyle = opts.color;
+		if (opts.color) this.setColor(opts.color);
 		if (opts.size) this.ctx.lineWidth = opts.size;
 
 		this.ctx.lineCap = "round";
@@ -388,32 +389,120 @@ DrawingBoard.Board.prototype = {
 	/**
 	 * set and get current drawing mode
 	 *
-	 * possible modes are "pencil" (draw normally) and "eraser" (draw transparent, like, erase, you know)
+	 * possible modes are "pencil" (draw normally), "eraser" (draw transparent, like, erase, you know), "filler" (paint can)
 	 */
 
-	setMode: function(mode, silent) {
+	setMode: function(newMode, silent) {
 		silent = silent || false;
-		this.mode = mode || 'pencil';
+		newMode = newMode || 'pencil';
+
+		this.ev.unbind('board:startDrawing', $.proxy(this.fill, this));
 
 		if (this.opts.eraserColor === "transparent")
-			this.ctx.globalCompositeOperation = this.mode === "eraser" ? "destination-out" : "source-over";
+			this.ctx.globalCompositeOperation = newMode === "eraser" ? "destination-out" : "source-over";
 		else {
-			if (this.mode === "eraser") {
-				this._prevColor = this.ctx.strokeStyle;
+			if (newMode === "eraser") {
 				if (this.opts.eraserColor === "background" && DrawingBoard.Utils.isColor(this.opts.background))
 					this.ctx.strokeStyle = this.opts.background;
 				else if (DrawingBoard.Utils.isColor(this.opts.eraserColor))
 					this.ctx.strokeStyle = this.opts.eraserColor;
-			} else if (this.mode === "pencil") {
-				this.ctx.strokeStyle = this._prevColor ? this._prevColor : this.opts.color;
+			} else if (!this.mode || this.mode === "eraser") {
+				this.ctx.strokeStyle = this.color;
 			}
+
+			if (newMode === "filler")
+				this.ev.bind('board:startDrawing', $.proxy(this.fill, this));
 		}
+		this.mode = newMode;
 		if (!silent)
 			this.ev.trigger('board:mode', this.mode);
 	},
 
 	getMode: function() {
 		return this.mode || "pencil";
+	},
+
+	setColor: function(color) {
+		var that = this;
+		color = color || this.color;
+		if (!DrawingBoard.Utils.isColor(color))
+			return false;
+		this.color = color;
+		if (this.opts.eraserColor !== "transparent" && this.mode === "eraser") {
+			var setStrokeStyle = function(mode) {
+				if (mode !== "eraser")
+					that.strokeStyle = that.color;
+				that.ev.unbind('board:mode', setStrokeStyle);
+			};
+			this.ev.bind('board:mode', setStrokeStyle);
+		} else
+			this.ctx.strokeStyle = this.color;
+	},
+
+	/**
+	 * Fills an area with the current stroke color.
+	 */
+	fill: function(e) {
+		var img = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+
+		// constants identifying pixels components
+		var INDEX = 0, X = 1, Y = 2, COLOR = 3;
+
+		// target color components
+		var stroke = this.ctx.strokeStyle;
+		var r = parseInt(stroke.substr(1, 2), 16);
+		var g = parseInt(stroke.substr(3, 2), 16);
+		var b = parseInt(stroke.substr(5, 2), 16);
+
+		// starting point
+		var start = DrawingBoard.Utils.pixelAt(
+			img,
+			parseInt( e.coords.x, 10),
+			parseInt( e.coords.y, 10)
+		);
+
+		// no need to continue if starting and target colors are the same
+		if (start[COLOR] === DrawingBoard.Utils.RGBToInt(r, g, b)) {
+			return;
+		}
+
+		// pixels to evaluate
+		var queue = [start];
+
+		// loop vars
+		var pixel, x, y;
+		var maxX = img.width - 1;
+		var maxY = img.height - 1;
+
+		while ((pixel = queue.pop())) {
+			if (pixel[COLOR] === start[COLOR]) {
+				img.data[pixel[INDEX]] = r;
+				img.data[pixel[INDEX] + 1] = g;
+				img.data[pixel[INDEX] + 2] = b;
+
+				// west
+				if (pixel[X] > 0) {
+					queue.push(DrawingBoard.Utils.pixelAt(img, pixel[X] - 1, pixel[Y]));
+				}
+
+				// east
+				if (pixel[X] < maxX) {
+					queue.push(DrawingBoard.Utils.pixelAt(img, pixel[X] + 1, pixel[Y]));
+				}
+
+				// north
+				if (pixel[Y] > 0) {
+					queue.push(DrawingBoard.Utils.pixelAt(img, pixel[X], pixel[Y] - 1));
+				}
+
+				// south
+				if (pixel[Y] < maxY) {
+					queue.push(DrawingBoard.Utils.pixelAt(img, pixel[X], pixel[Y] + 1));
+				}
+			}
+		}
+
+		this.ctx.putImageData(img, 0, 0);
 	},
 
 
@@ -602,7 +691,7 @@ DrawingBoard.Utils.tpl = (function(){
 /**
  * https://github.com/jeromeetienne/microevent.js
  * MicroEvent - to make any js object an event emitter (server or browser)
- * 
+ *
  * - pure javascript - server compatible, browser compatible
  * - dont rely on the browser doms
  * - super simple - you get it immediatly, no mistery, no magic involved
@@ -661,8 +750,38 @@ DrawingBoard.Utils.boxBorderHeight = function($el, withPadding, withMargin) {
 };
 
 DrawingBoard.Utils.isColor = function(string) {
-	console.log(string);
+	if (!string || !string.length) return false;
 	return (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i).test(string) || $.inArray(string.substring(0, 3), ['rgb', 'hsl']) !== -1;
+};
+
+/**
+ * Packs an RGB color into a single integer.
+ */
+DrawingBoard.Utils.RGBToInt = function(r, g, b) {
+	var c = 0;
+	c |= (r & 255) << 16;
+	c |= (g & 255) << 8;
+	c |= (b & 255);
+	return c;
+};
+
+/**
+ * Returns informations on the pixel located at (x,y).
+ */
+DrawingBoard.Utils.pixelAt = function(image, x, y) {
+	var i = (y * image.width + x) * 4;
+	var c = DrawingBoard.Utils.RGBToInt(
+		image.data[i],
+		image.data[i + 1],
+		image.data[i + 2]
+	);
+
+	return [
+		i, // INDEX
+		x, // X
+		y, // Y
+		c  // COLOR
+	];
 };
 
 (function() {
