f9e94a1a29bdec62284135b09585cc629aa974fe
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-09-30 15:32:26 +0200

    removing unused google analytics script (testing commits bug)

--- example/script.js
+++ example/script.js
@@ -13,8 +13,3 @@ $('script[data-example]').each(function(n, item) {
 			'</pre></code>'
 		);
 });
-
-var _gaq=[['_setAccount','UA-13184829-2'],['_trackPageview']];
-(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-s.parentNode.insertBefore(g,s)}(document,'script'));
